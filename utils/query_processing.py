1dd95cae15e9bbaaa48f8dcd318195c6fd868398
vishnu.va <vishnu.va@pitsolutions.com>
2024-08-21 21:10:12 +0530

    conversation history initial commit

--- utils/query_processing.py
+++ utils/query_processing.py
@@ -6,8 +6,21 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from utils.llm_selection import get_llm, get_embedding_model
 from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 import streamlit as st
 
+### Statefully manage chat history ###
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 def process_query(query: str, model_choice):
     query_vector = get_query_embedding(query, model_choice)
@@ -19,30 +32,57 @@ def process_query(query: str, model_choice):
     retriever = vectorstore.as_retriever()
     # Prepare the context by joining the relevant chunks
     context = "\n\n".join([chunk.page_content for chunk in relevant_chunks])
-    st.write(context)
-    def format_docs(relevant_chunks):
-        return "\n\n".join(chunk.page_content for chunk in relevant_chunks)
-    
-    template = """Use the following pieces of context to answer the question at the end.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    Use three sentences maximum and keep the answer as concise as possible.
-    Always say "thanks for asking!" at the end of the answer.
-
-    {context}
-
-    Question: {question}
-
-    Helpful Answer:"""
-    custom_rag_prompt = PromptTemplate.from_template(template)
-
     llm = get_llm(model_choice)
-
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | custom_rag_prompt
-        | llm
-        | StrOutputParser()
-    )
     
-    response = rag_chain.invoke(query)
+    ### Contextualize question ###
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    ### Answer question ###
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+    Use three sentences maximum and keep the answer concise.\
+
+    {context}"""
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    response = conversational_rag_chain.invoke(
+        {"input": query},
+        config={"configurable": {"session_id": "ragconv"}},
+    )["answer"]
+
     return response
